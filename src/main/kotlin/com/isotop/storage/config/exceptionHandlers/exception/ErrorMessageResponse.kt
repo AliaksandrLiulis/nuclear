9d5d8e54f931b11fd4e39a313ce5bf548c51f8a3
Aliaksandr <AliaksandrLiulis@gmail.com>
2019-10-13 02:12:45 +0300

    added error code

--- src/main/kotlin/com/isotop/storage/config/exceptionHandlers/exception/ErrorMessageResponse.kt
+++ src/main/kotlin/com/isotop/storage/config/exceptionHandlers/exception/ErrorMessageResponse.kt
@@ -1,6 +1,6 @@
 package com.isotop.storage.config.exceptionHandlers.exception
 
 data class ErrorMessageResponse(
-    val error: Boolean,
-    val message: String?
+    val errorCode: Boolean,
+    val errorMessage: String?
 )
