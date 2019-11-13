4dafafc6b8b63bd8f91b83cc4b23fa94b7f911b5
Aliaksandr <AliaksandrLiulis@gmail.com>
2019-11-14 02:37:45 +0300

    changed mapping

--- src/main/kotlin/com/isotop/storage/controller/ContainerController.kt
+++ src/main/kotlin/com/isotop/storage/controller/ContainerController.kt
@@ -1,9 +1,7 @@
 package com.isotop.storage.controller
 
 import com.isotop.storage.dto.request.AddContainerRequest
-import com.isotop.storage.dto.request.AddContainerToStorageRequest
 import com.isotop.storage.dto.response.ListContainerDataResponse
-import com.isotop.storage.dto.response.StorageResponse
 import com.isotop.storage.service.ContainerService
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.*
@@ -30,8 +28,19 @@ open class ContainerController(
     }
 
     @PreAuthorize("hasRole('STORAGE')")
-    @PostMapping("sources",produces = ["application/json"])
+    @PostMapping(produces = ["application/json"])
     open fun addContainer(
+        @RequestBody
+        payload: AddContainerRequest
+    ) {
+        payload.moutionType = 1
+        payload.sourceTypeCode = 2
+        containerService.addContainer(payload)
+    }
+
+    @PreAuthorize("hasRole('STORAGE')")
+    @PutMapping("sources",produces = ["application/json"])
+    open fun updateContainer(
         @RequestBody
         payload: AddContainerRequest
     ) {
