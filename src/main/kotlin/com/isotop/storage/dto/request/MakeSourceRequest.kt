04ab205d2ce94b2a59233dd8f6472c296ca8633a
Aliaksandr <AliaksandrLiulis@gmail.com>
2019-11-30 01:49:25 +0300

    removed make data

--- src/main/kotlin/com/isotop/storage/dto/request/MakeSourceRequest.kt
+++ src/main/kotlin/com/isotop/storage/dto/request/MakeSourceRequest.kt
@@ -1,11 +1,8 @@
 package com.isotop.storage.dto.request
 
-import java.time.LocalDate
-
 data class MakeSourceRequest (
     val passportNumber: String,
     val serialNumber: String,
-    val makeDate: LocalDate?,
     val makeTypeCode: Int,
     val nuclideTypeCode: Int
 )
