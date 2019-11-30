7b7bbede7ce823c19d5a04c8d719db8005eedd39
Aliaksandr <AliaksandrLiulis@gmail.com>
2019-12-01 01:58:12 +0300

    added nuclidTypeCode

--- src/main/kotlin/com/isotop/storage/dto/response/PackageResponse.kt
+++ src/main/kotlin/com/isotop/storage/dto/response/PackageResponse.kt
@@ -12,6 +12,7 @@ data class PackageResponse(
     val packageCode: Int,
     val openSourceUsing: Int,
     val sourceActivity: Double,
+    val nuclideTypeCode: String,
     val nuclideType: String,
     val containerChipher: String,
     val sourceDiametr: Double,
